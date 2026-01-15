24bcd4b8d313717e72b2ee677217c7cf8160370c
jansulzberger <jan.sulzberger@paulparey.de>
2026-01-15 14:01:10 +0100

    Zwischenstand

--- pages/feed.php
+++ pages/feed.php
@@ -3,10 +3,33 @@
 
 include '../../../include/db.php';
 
-$feedname = array_key_exists('name',$_GET) ? $_GET['name'] : '';
 
-$resources = get_resources();
+if(!array_key_exists('name',$_GET)){
+    return 'parameter name missing';
+}
 
-//$videoModel = new \model\VideoModel();
+$feedname = $_GET['name'];
+// Get the Node ID for "Ja" in Field 120
+$ja_node = get_node_id("Ja", 120);
+// Get the Node ID for "DJZ" in Field 108
+$djz_node = get_node_id(strtoupper($feedname), 108);
 
-print $feedname;
+$search_query = "@@" . $ja_node . " @@" . $djz_node . " !field130:*";
+$resource_types = "3"; // Photography only
+$order_by = "resourceid";
+$archive_state = 0; // Only active resources
+$limit = 5;
+$sort_direction = "DESC";
+
+// Execute the search
+$results = do_search($search_query, $resource_types, $order_by, $archive_state, $limit, $sort_direction);
+
+// Check if we have results
+if (is_array($results) && count($results) > 0) {
+    foreach ($results as $resource) {
+        echo "Found Resource ID: " . $resource['ref'] . "<br>";
+        echo "Title: " . $resource['field8'] . "<br>"; // field8 is usually Title
+    }
+} else {
+    echo "No resources found matching the criteria.";
+}
